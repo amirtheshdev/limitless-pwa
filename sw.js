b9cfb16e1d9262a29f8d652dbb18f8bb48d5485d
amirtheshdev <amirthesh5707@gmail.com>
2026-08-17 10:30:33 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "limitless-v1";
+const ASSETS = [
+  "index.html", "manifest.json", "icon-192.png", "icon-512.png",
+  "css/style.css",
+  "js/state.js", "js/transactions.js", "js/speech.js", "js/auth.js", "js/ui.js", "js/app.js"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
